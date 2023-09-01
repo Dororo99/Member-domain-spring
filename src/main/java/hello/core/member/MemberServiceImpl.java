4bf13bb0764f39af1e2f760ff2b68ca382af6d55
Dororo99 <99dororo@gmail.com>
2023-09-01 17:19:21 +0900

    feat: Pure java & OCP DIP false

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,9 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // override by polymorphism
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); // OCP & DIP (X)
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // DIP (X): 고수준인 interface가 저수준인 class에 의존하고 있음
+    // OCP (X): 기능 확장시 코드를 수정해야 함! 의존중이기 때문임)
 
     @Override
     public void join(Member member) {
