6c4e4367b236b24f80e2cc150c339207f6a715a0
Dororo99 <99dororo@gmail.com>
2023-09-02 15:41:38 +0900

    fix: DIP rules

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,22 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.FixDiscountPolicy;
+//import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 얘를 Rate로 바꿔줘야함
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    // DIP 위반: 구체(구현) 클래스에 의존 중
+//    // OCP 위반: 이런식으로 코드를 아예 바꿔줘야 함!
+
+    // DIP 위반을 막자
+    private DiscountPolicy discountPolicy; // interface에만 의존함
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findByID(memberId);
