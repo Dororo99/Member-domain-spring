42bb1a61960dbc859bef4e223e28f14c48b25a18
Dororo99 <99dororo@gmail.com>
2023-09-05 14:07:30 +0900

    feat: Update singleton with stateless

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -17,16 +17,17 @@ class StatefulServiceTest {
 
 
         //ThreadA: A가 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000); // 지역 변수를 이용하여 문제를 해결하면 된다!
         //ThreadB: B가 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: A가 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000); // 이렇게 하면 20000원이 출력됨!
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000); // 이렇게 하면 20000원이 출력됨!
         // 10000 -> 20000으로 변경된다
+        // 무상태로 설계해야 한다!
     }
 
     static class TestConfig {
