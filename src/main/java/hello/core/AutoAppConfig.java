29a4fc322d6ef370a518df00522bb7ba8f943dbf
Dororo99 <99dororo@gmail.com>
2023-09-11 16:11:57 +0900

    feat: Auto vs direct register

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -16,8 +16,9 @@ import org.springframework.context.annotation.FilterType;
         // AppConfig안의 Bean이 이중등록 될수 있어서 빼기 위함! (기존 예제 코드를 남기고 유지하기 위해서)
 ) // 자동으로 컴포넨트를 긁어오는거
 public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    } // 이렇게 하면 자동 빈보다 수동 빈이 우선권을 갖기 때문에 수동빈이 자동빈을 오버라이딩한다.
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    } // 이렇게 하면 자동 빈보다 수동 빈이 우선권을 갖기 때문에 수동빈이 자동빈을 오버라이딩한다.
+    // 이런식으로 개발하면 수동과 자동이 충돌을 일으키므로 spring boot가 동작하지 않는다
 }
